0c31054a625036b342e00be392a3b12b7686db4c
Matthew Weisfeld <matthewedwardweisfeld@gmail.com>
2023-06-14 19:29:37 -0400

    Day 3 Any Number Of Containers

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs::read_to_string, path::Path};
+use std::{collections::HashSet, fs::read_to_string, ops::ControlFlow, path::Path};
 
 trait Priority {
     fn to_priority(&self) -> Option<u32>;
@@ -47,13 +47,53 @@ impl Rucksack {
             priorities + char.to_priority().unwrap_or_default()
         })
     }
+
+    fn get_packing_errors_priority_anysize(&self) -> u32 {
+        let mut errors: HashSet<char> = HashSet::new();
+        let mut compartment_sets: Vec<HashSet<char>> = Vec::new();
+
+        self.compartments.iter().skip(1).for_each(|compartment| {
+            let compartment_set = HashSet::from_iter(compartment.contents.iter().cloned());
+            compartment_sets.push(compartment_set);
+        });
+
+        self.compartments
+            .iter()
+            .enumerate()
+            .for_each(|(index, compartment)| {
+                compartment.contents.iter().for_each(|char| {
+                    compartment_sets
+                        .iter()
+                        .skip(index)
+                        .try_for_each(|compartment_set| {
+                            if compartment_set.contains(char) {
+                                errors.insert(*char);
+                                return ControlFlow::Break(());
+                            }
+                            ControlFlow::Continue(())
+                        });
+                });
+            });
+
+        errors.iter().fold(0, |priorities, char| {
+            priorities + char.to_priority().unwrap_or_default()
+        })
+    }
 }
 
-fn read_rucksacks_contents(path: &Path) -> Vec<Rucksack> {
+fn read_rucksacks_contents(
+    path: &Path,
+    rucksack_container_count: usize,
+) -> Result<Vec<Rucksack>, String> {
     let mut rucksacks: Vec<Rucksack> = Vec::new();
 
     for line in read_to_string(path).unwrap().lines() {
-        let compartment_size = line.len() / 2;
+        if line.len() % rucksack_container_count != 0 {
+            return Err(format!(
+                "A rucksack is not divisible into {rucksack_container_count} equal containers"
+            ));
+        }
+        let compartment_size = line.len() / rucksack_container_count;
         let chars: Vec<char> = line.chars().collect();
 
         let mut rucksack = Rucksack {
@@ -69,14 +109,14 @@ fn read_rucksacks_contents(path: &Path) -> Vec<Rucksack> {
         rucksacks.push(rucksack);
     }
 
-    rucksacks
+    Ok(rucksacks)
 }
 
 pub fn day3() {
-    let rucksacks = read_rucksacks_contents(Path::new("rucksacks-contents.txt"));
+    let rucksacks = read_rucksacks_contents(Path::new("rucksacks-contents.txt"), 2).unwrap();
     let priorities: Vec<u32> = rucksacks
         .iter()
-        .map(|rucksack| rucksack.get_packing_errors_priority())
+        .map(|rucksack| rucksack.get_packing_errors_priority_anysize())
         .collect();
 
     let priorities_sum: u32 = priorities.iter().sum();
